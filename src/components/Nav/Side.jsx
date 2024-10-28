2aa365de24eedf7da9ef1da8afc676d48aad8be3
jota <jvfranco08@gmail.com>
2024-10-28 10:56:06 -0300

    adiconando conteudo na sidebar

--- src/components/Nav/Side.jsx
+++ src/components/Nav/Side.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import {Sidebar,Menu,MenuItem,SubMenu,sidebarClasses} from 'react-pro-sidebar'
+import {Link} from 'react-router-dom'
 
-const side = () => {
+export default function Side() {
   return (
-    <div>side</div>
+    <Sidebar 
+
+      rootStyles={{
+        [`.${sidebarClasses.container}`]:{
+          backgroundColor: 'white', color:'black'
+        }
+      }}
+    
+    >
+        <Menu>
+          <SubMenu label="Dasboard">
+            <MenuItem component={<Link to="/"/>}>Home</MenuItem>
+            <MenuItem component={<Link to="/Exemplo1"/>}>Exemplo-1</MenuItem>
+            <MenuItem component={<Link to="/Exemplo2"/>}>Exemplo-2</MenuItem>
+          </SubMenu>
+          <SubMenu label="Conta">
+            <MenuItem>Minha Conta</MenuItem>
+            <MenuItem>Sair</MenuItem>
+          </SubMenu>
+        </Menu>
+    </Sidebar>
   )
 }
-
-export default side
